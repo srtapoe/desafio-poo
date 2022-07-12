8632ebd0c37c98c7a6f1deae0b122b24cfdb917b
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2022-07-12 11:27:59 -0300

    Modificadas classes Bootcamp e Dev

--- src/main/java/org/desafio/dominio/Bootcamp.java
+++ src/main/java/org/desafio/dominio/Bootcamp.java
@@ -1,11 +1,36 @@
 package org.desafio.dominio;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
+@Getter
+@Setter
 public class Bootcamp {
 
     private String nome;
-    private Set<Conteudos> conteudosInscritos = new LinkedHashSet<>();
-    private Set<Conteudos> conteudosConcluidos = new LinkedHashSet<>();
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsNoBootcamp = new HashSet<>();
+    private Set<Conteudos> conteudosBootcamp = new LinkedHashSet<>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsNoBootcamp, bootcamp.devsNoBootcamp) && Objects.equals(conteudosBootcamp, bootcamp.conteudosBootcamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsNoBootcamp, conteudosBootcamp);
+    }
 }
--- src/main/java/org/desafio/dominio/Dev.java
+++ src/main/java/org/desafio/dominio/Dev.java
@@ -1,4 +1,36 @@
 package org.desafio.dominio;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
 public class Dev {
+
+    private String nome;
+    private Set<Conteudos> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudos> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
 }
